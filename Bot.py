38be702501523dbc0c7334f8afad7a49afd8b547
Lunkers <freddelunk@gmail.com>
2017-07-21 17:26:41 +0200

    Add files to repo

---
+++ Bot.py
@@ -0,0 +1,29 @@
+import discord
+from discord.ext import commands
+import asyncio
+import os
+import random
+
+description = "Simple meme bot."
+
+bot = commands.Bot(command_prefix="please", description=description)
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+def meme():
+    meme_list = os.listdir("./memes")
+    rand_numb = random.randint(0, len(meme_list)-1)
+    return meme_list[rand_numb]
+
+@bot.event
+async def on_message(message):
+    if message.content.startswith("please meme"):
+        m = meme()
+        await bot.send_file(message.channel,fp="./memes/"+m ,filename=m, content="aah, fresh meme!")
+
+bot.run("MjgxNzgyNDg0ODA1NDg0NTQ0.C9JXrA.VisWCc4R2_6EyrBxnk-Alogc1mU")
